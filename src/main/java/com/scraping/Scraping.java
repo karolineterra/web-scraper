fa54cf7c0b177374fc164e38f76c28923ae20e1a
karolineterra <terra.karoline1@gmail.com>
2023-10-05 14:39:32 -0300

    Adicionei uma verificação para o caso dos valores ficarem vazios

--- src/main/java/com/scraping/Scraping.java
+++ src/main/java/com/scraping/Scraping.java
@@ -20,34 +20,52 @@ public class Scraping {
     private void scrapeTitle() {
         Elements title = pagePath.select("[data-productname]");
         String textTitle = title.text();
-        System.out.println("---- NOME DO PRODUTO ----");
-        System.out.println(textTitle);
-        new WriteFile(textTitle).startWriting();
+        if (textTitle.isEmpty()) {
+            System.out.println("O nome do produto não foi encontrado");
+        } else {
+            System.out.println("---- NOME DO PRODUTO ----");
+            System.out.println(textTitle);
+            new WriteFile(textTitle).startWriting();
+        }
 
     }
 
     private void scrapePrice() {
         Elements price = pagePath.select(".default-price span strong");
         String textPrice = price.text();
-        System.out.println("----- PREÇO DO PRODUTO ----");
-        System.out.println(textPrice);
-        new WriteFile(textPrice).startWriting();
+        if (textPrice.isEmpty()) {
+            System.out.println("O preço do produto não foi encontrado");
+        } else {
+            System.out.println("----- PREÇO DO PRODUTO ----");
+            System.out.println(textPrice);
+            new WriteFile(textPrice).startWriting();
+        }
+
     }
 
     private void scrapeImage() {
         Elements imagePath = pagePath.select(".photo-figure .zoom");
         String imgSrc = imagePath.attr("src");
-        System.out.println("---- LINK DA IMAGEM DO PRODUTO ----");
-        System.out.println(imgSrc);
-        new WriteFile(imgSrc).startWriting();
+
+        if (imgSrc.isEmpty()) {
+            System.out.println("A imagem do produto não foi encontrada");
+        } else {
+            System.out.println("---- LINK DA IMAGEM DO PRODUTO ----");
+            System.out.println(imgSrc);
+            new WriteFile(imgSrc).startWriting();
+        }
     }
 
     private void scrapeDescription() {
         Elements description = pagePath.select("#features p");
         String textDescription = description.text();
-        System.out.println("---- DESCRIÇÃO DO PRODUTO ----");
-        System.out.println(textDescription);
-        new WriteFile(textDescription).startWriting();
+        if (textDescription.isEmpty()) {
+            System.out.println("O preço do produto não foi encontrado");
+        } else {
+            System.out.println("---- DESCRIÇÃO DO PRODUTO ----");
+            System.out.println(textDescription);
+            new WriteFile(textDescription).startWriting();
+        }
     }
 
 }
